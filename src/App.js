c7d2412513765e1cd158a40f713acd8a4ca40532
the-realgabriel <theopgabrie@gmail.com>
2024-05-23 17:56:25 +0100

    modified:   package-lock.json 	modified:   src/App.js 	modified:   src/pages/profile.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './App.css';
 import Home from './pages/home';
 
+
+
 function App() {
   return (
     <div className="App"> 
     <Home />
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+    </Routes>
+
     </div>
   );
 }
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 
 function Profile() {
   const { userId } = useParams();
